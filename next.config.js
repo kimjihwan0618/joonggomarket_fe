e9720e18ca353743ff588b80981993a4be1d65f6
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-11-17 23:55:48 +0900

    배포 테스트

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ module.exports = {
   // 아래 주소들만 out 폴더에 만들어줘 ! => getServerSideProps 있는 페이지는 제외
   exportPathMap: () => ({
     '/': { page: '/' },
-    '/boards': { page: '/boards' },
-    '/boards/new': { page: '/boards/new' },
-    '/markets': { page: '/markets' },
-    '/markets/new': { page: '/markets/new' },
+    // '/boards': { page: '/boards' },
+    // '/boards/new': { page: '/boards/new' },
+    // '/markets': { page: '/markets' },
+    // '/markets/new': { page: '/markets/new' },
     '/login': { page: '/login' },
     '/signup': { page: '/signup' },
     '/mypage/market': { page: '/mypage/market' },
