d4dc1ec1c7818aaa4605b75020e7db3ec9759aa1
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-12-17 17:41:53 +0900

    url 클립보드 복사 실패 문구 표시 추가

--- src/components/commons/hooks/custom/useTextCopy.tsx
+++ src/components/commons/hooks/custom/useTextCopy.tsx
@@ -3,14 +3,19 @@ import { Modal } from 'antd'
 export const useTextCopy = () => {
   const onCopyLink = (): void => {
     const url = window.location.href
-    navigator.clipboard
-      .writeText(url)
-      .then(() => {
-        Modal.success({ content: 'URL이 클립보드에 복사되었습니다!' })
-      })
-      .catch((err) => {
-        Modal.error({ content: '클립보드 복사에 실패했습니다.' })
-      })
+    if (navigator.clipboard) {
+      // 추가된 코드
+      navigator.clipboard
+        .writeText(url)
+        .then(() => {
+          Modal.success({ content: 'URL이 클립보드에 복사되었습니다!' })
+        })
+        .catch((err) => {
+          Modal.error({ content: '클립보드 복사에 실패했습니다.' })
+        })
+    } else {
+      Modal.error({ content: 'URL 복사는 HTTPS 환경에서만 지원됩니다.' }) // 추가된 코드
+    }
   }
   return {
     onCopyLink,
